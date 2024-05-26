ef04a81ea5681ba151dd504b1522a8f42115cd74
PhatNT <phatnt0302@gmail.com>
2024-05-27 00:21:12 +0700

    chore: hot fix

--- src/components/organisms/Appbar.tsx
+++ src/components/organisms/Appbar.tsx
@@ -58,63 +58,57 @@ const Appbar: React.FC<Props> = ({ variant }) => {
   };
 
   return (
-    <>
-      <AppBar sx={{ height: APPBAR.height }} color="default" elevation={0}>
-        <Toolbar>
-          <Link href={PATH.home} variant="h6" underline="none" color="common.black">
-            {APP_NAME}
-          </Link>
-          {isDetail && (
-            <>
-              <Stack direction="row-reverse" flexGrow={1}>
-                <Stack direction="row" spacing={1}>
-                  <IconButton onClick={onSettingClick} size="small">
-                    <Iconify icon="iconamoon:menu-burger-horizontal" width={20} height={20} />
-                  </IconButton>
-                </Stack>
+    <AppBar sx={{ height: APPBAR.height }} color="default" elevation={0}>
+      <Toolbar>
+        <Link href={PATH.home} variant="h6" underline="none" color="common.black">
+          {APP_NAME}
+        </Link>
+        {isDetail && (
+          <>
+            <Stack direction="row-reverse" flexGrow={1}>
+              <Stack direction="row" spacing={1}>
+                <IconButton onClick={onSettingClick} size="small">
+                  <Iconify icon="iconamoon:menu-burger-horizontal" width={20} height={20} />
+                </IconButton>
               </Stack>
-              <MenuPopover
-                open={openSetting}
-                anchorEl={anchorElSetting}
-                onClose={handleSettingClose}
-              >
-                <Box sx={{ my: 1.5, px: 2.5, display: 'flex', gap: 1, alignItems: 'center' }}>
-                  <Iconify
-                    icon="iconamoon:profile"
-                    width={15}
-                    height={15}
-                    sx={{ color: isLight ? theme.palette.grey[600] : theme.palette.grey[400] }}
-                  />
-                  <Typography variant="subtitle2" color="text.secondary">
-                    {credential?.username || 'Admin'}
-                  </Typography>
-                </Box>
-                <Divider sx={{ borderStyle: 'dashed' }} />
-                <Stack sx={{ p: 1 }}>
-                  {SETTING_OPTIONS.map((option: TOption) => (
-                    <MenuItem
-                      key={option.value}
-                      onClick={() => handleSettingSelect(option)}
-                      sx={{ borderRadius: 2 }}
-                    >
-                      {option.value}
-                    </MenuItem>
-                  ))}
-                </Stack>
-                <Divider sx={{ borderStyle: 'dashed' }} />
-                <Box p={1}>
-                  <MenuItem onClick={onSignOut} sx={{ borderRadius: 2 }}>
-                    <Typography variant="subtitle2" color="text.secondary">
-                      Logout
-                    </Typography>
+            </Stack>
+            <MenuPopover open={openSetting} anchorEl={anchorElSetting} onClose={handleSettingClose}>
+              <Box sx={{ my: 1.5, px: 2.5, display: 'flex', gap: 1, alignItems: 'center' }}>
+                <Iconify
+                  icon="iconamoon:profile"
+                  width={15}
+                  height={15}
+                  sx={{ color: isLight ? theme.palette.grey[600] : theme.palette.grey[400] }}
+                />
+                <Typography variant="subtitle2" color="text.secondary">
+                  {credential?.username || 'Admin'}
+                </Typography>
+              </Box>
+              <Divider sx={{ borderStyle: 'dashed' }} />
+              <Stack sx={{ p: 1 }}>
+                {SETTING_OPTIONS.map((option: TOption) => (
+                  <MenuItem
+                    key={option.value}
+                    onClick={() => handleSettingSelect(option)}
+                    sx={{ borderRadius: 2 }}
+                  >
+                    {option.value}
                   </MenuItem>
-                </Box>
-              </MenuPopover>
-            </>
-          )}
-        </Toolbar>
-      </AppBar>
-    </>
+                ))}
+              </Stack>
+              <Divider sx={{ borderStyle: 'dashed' }} />
+              <Box p={1}>
+                <MenuItem onClick={onSignOut} sx={{ borderRadius: 2 }}>
+                  <Typography variant="subtitle2" color="text.secondary">
+                    Logout
+                  </Typography>
+                </MenuItem>
+              </Box>
+            </MenuPopover>
+          </>
+        )}
+      </Toolbar>
+    </AppBar>
   );
 };
 
